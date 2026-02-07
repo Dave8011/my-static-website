09ea60c4dfd7455ad3d4bd43b3ff595de8291633
Dave <dave@example.com>
2026-02-07 10:12:55 +0530

    Add new doctors, fix footer consistency and mobile alignment

--- js/doctors_data.js
+++ js/doctors_data.js
@@ -26,5 +26,33 @@ const doctorsData = [
         specialty: "Occupational Therapist (Gold Medalist)",
         image: "images/dr-sakshi.jpg",
         profileLink: "profiles/dr-sakshi.html"
+    },
+    {
+        id: "nidhi",
+        name: "Ms. Nidhi Parekh",
+        specialty: "Psychologist (M.A. in Psychology)",
+        image: "images/ms-nidhi.jpg",
+        profileLink: "profiles/ms-nidhi.html"
+    },
+    {
+        id: "chetan",
+        name: "Dr. Chetan Rambhia",
+        specialty: "Yoga Instructor and Homeopath",
+        image: "images/dr-chetan.jpg",
+        profileLink: "profiles/dr-chetan.html"
+    },
+    {
+        id: "raj",
+        name: "Dr. Raj Gada",
+        specialty: "Physiotherapist (BPTh, COMT)",
+        image: "images/dr-raj.jpg",
+        profileLink: "profiles/dr-raj.html"
+    },
+    {
+        id: "gurmeet",
+        name: "Dr. Gurmeet Kaur Bachher",
+        specialty: "Speech Therapist (46+ Years Experience)",
+        image: "images/dr-gurmeet.jpg",
+        profileLink: "profiles/dr-gurmeet.html"
     }
 ];
